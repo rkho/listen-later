d78a486513a7fa42151f1d7033097d0973c4cd2a
Pavan Ravipati <psravipati@gmail.com>
2015-07-08 14:01:09 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// node modules
+// ============
+
+var gulp = require('gulp');
+
+//TODO:
+//One script tag for all bower installed files
+  //Gulp everything in our bower directory and minify / uglify it down into our dist / vendor file
+//One script for all of our src js
+  //Gulp everythign in our build/js and build/css into one dist file
+
